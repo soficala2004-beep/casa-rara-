9fbd730b9ddb305175346f0d2b5b51564567f003
soficala2004-beep <soficala2004@gmail.com>
2025-09-16 16:35:29 -0300

    Add files via upload

---
+++ project-title/app-files/data.js
@@ -0,0 +1,41 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ejemplos-de-vdeos-360",
+      "name": "ejemplos-de-vídeos-360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8501068534155536,
+          "pitch": -0.1665975495761174,
+          "rotation": 2.356194490192345,
+          "target": "0-ejemplos-de-vdeos-360"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
